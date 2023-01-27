35e3c2da4b52a3643ca607be1e2553efdbf2ff70
M. Zainul Hakim <70321824+zaynhakim@users.noreply.github.com>
2023-01-27 14:26:28 +0700

    cukup oke

--- main.js
+++ main.js
@@ -1,37 +1,69 @@
-import './style.css'
-import javascriptLogo from './javascript.svg'
-import { setupCounter } from './counter.js'
+import "./style.css";
 
-document.querySelector('#app').innerHTML = `
+document.querySelector("#app").innerHTML = `
 <div class="relative flex min-h-screen flex-col justify-center overflow-hidden bg-gray-50 py-6 sm:py-12">
-<img src="/img/beams.jpg" alt="" class="absolute top-1/2 left-1/2 max-w-none -translate-x-1/2 -translate-y-1/2" width="1308" />
-<div class="absolute inset-0 bg-[url(/img/grid.svg)] bg-center [mask-image:linear-gradient(180deg,white,rgba(255,255,255,0))]"></div>
-<div class="relative bg-gradient-to-tl from-teal-500/25 px-6 pt-10 pb-8 text-gray-900 shadow-xl ring-1 ring-gray-900/5 sm:mx-auto sm:max-w-lg sm:rounded-lg sm:px-10">
-  <div class="mx-auto max-w-md">
-    <div class="flex flex-row items-center gap-3">
-      <div>Logo</div>
-      <header class="drop-shadow-sm">
-        <h1 class="text-2xl font-semibold text-cyan-600">Panitia Penerimaan Santri Baru</h1>
-        <h2 class="text-3xl font-bold text-cyan-600">SMP-SMA ALI MAKSUM</h2>
-        <div class="text-lg font-medium">Tahun Pelajaran 2023/2024</div>
-      </header>
-    </div>
-    <div class="mt-8 rounded-xl border bg-gray-50/70 p-6">
-      <h3 class="text-center text-2xl font-bold">PANITIA</h3>
-      <div class="my-4 grid grid-flow-row gap-4">
-        <div class="grid grid-flow-col grid-cols-6 items-center">
-          <div class="col-span-2 text-xl">Nama</div>
-          <div class="col-span-4 h-12 rounded-2xl bg-white p-2 text-xl"></div>
+  <img src="/img/beams.jpg" alt="" class="absolute top-1/2 left-1/2 max-w-none -translate-x-1/2 -translate-y-1/2" width="1308" />
+  <div class="absolute inset-0 bg-center [mask-image:linear-gradient(180deg,white,rgba(255,255,255,0))]"></div>
+  <div class="relative mx-auto max-w-lg rounded-xl bg-gradient-to-tl from-indigo-500/50 via-sky-200/40 to-gray-50/90 px-6 pt-10 pb-8 text-gray-900 shadow-xl ring-1 ring-gray-900/5 sm:px-10">
+    <div class="mx-auto max-w-md">
+      <div class="flex flex-row items-center gap-3">
+        <div>
+          <svg xmlns="http://www.w3.org/2000/svg" xml:space="preserve" width="80px" height="80px" version="1.1" style="shape-rendering:geometricPrecision; text-rendering:geometricPrecision; image-rendering:optimizeQuality; fill-rule:evenodd; clip-rule:evenodd" viewBox="0 0 12.57 12.56" xmlns:xlink="http://www.w3.org/1999/xlink" xmlns:xodm="http://www.corel.com/coreldraw/odm/2003">
+            <defs>
+              <style type="text/css">
+                <![CDATA[
+                 .str0 {stroke:#178E9E;stroke-width:0.09;stroke-miterlimit:22.9256}
+                 .fil0 {fill:none}
+                 .fil3 {fill:none;fill-rule:nonzero}
+                 .fil2 {fill:#178E9E}
+                 .fil1 {fill:#178E9E;fill-rule:nonzero}
+                ]]>
+              </style>
+            </defs>
+            <g id="Layer_x0020_1">
+              <metadata id="CorelCorpID_0Corel-Layer" />
+              <g id="_1510571350528">
+                <rect class="fil0" width="12.57" height="12.56" />
+                <g>
+                  <g>
+                    <path class="fil1" d="M9.8 10.96l-6.99 0 0 -0.24 6.99 0 0.09 0.2 -0.09 0.04zm0.09 -0.04l-0.04 0.04 -0.05 0 0.09 -0.04zm-3.47 -10.17l-0.24 -0.03 0.24 0 0.07 0.38 0.1 0.36 0.14 0.35 0.16 0.33 0.19 0.33 0.2 0.32 0.23 0.3 0.24 0.3 0.26 0.29 0.26 0.29 0.27 0.28 0.27 0.27 0.27 0.27 0.27 0.28 0.26 0.27 0.25 0.27 0.24 0.27 0.23 0.27 0.2 0.28 0.18 0.29 0.16 0.29 0.12 0.3 0.1 0.31 0.05 0.32 0.02 0.33 -0.03 0.34 -0.07 0.36 -0.13 0.36 -0.18 0.38 -0.23 0.39 -0.29 0.4 -0.34 0.42 -0.18 -0.16 0.34 -0.4 0.28 -0.39 0.22 -0.37 0.16 -0.36 0.12 -0.34 0.07 -0.32 0.02 -0.31 -0.01 -0.3 -0.05 -0.29 -0.09 -0.29 -0.11 -0.28 -0.15 -0.27 -0.17 -0.27 -0.2 -0.27 -0.22 -0.27 -0.23 -0.27 -0.25 -0.26 -0.26 -0.27 -0.27 -0.27 -0.27 -0.27 -0.27 -0.28 -0.27 -0.28 -0.27 -0.29 -0.26 -0.3 -0.24 -0.3 -0.24 -0.32 -0.21 -0.33 -0.2 -0.34 -0.17 -0.35 -0.14 -0.37 -0.11 -0.38 -0.07 -0.4 0.23 0zm-3.61 9.97l0 0.24 -0.09 -0.04 -0.36 -0.41 -0.29 -0.4 -0.24 -0.38 -0.18 -0.38 -0.13 -0.36 -0.07 -0.35 -0.03 -0.34 0.01 -0.33 0.06 -0.33 0.09 -0.31 0.13 -0.3 0.16 -0.3 0.18 -0.28 0.21 -0.29 0.22 -0.28 0.24 -0.27 0.26 -0.28 0.26 -0.27 0.28 -0.28 0.27 -0.27 0.27 -0.28 0.28 -0.28 0.26 -0.29 0.26 -0.29 0.24 -0.3 0.23 -0.31 0.2 -0.31 0.19 -0.33 0.16 -0.33 0.13 -0.34 0.11 -0.36 0.06 -0.37 0.24 0.03 -0.07 0.39 -0.11 0.38 -0.14 0.36 -0.17 0.35 -0.2 0.34 -0.21 0.33 -0.24 0.31 -0.24 0.31 -0.26 0.3 -0.27 0.29 -0.28 0.28 -0.27 0.28 -0.28 0.28 -0.27 0.27 -0.26 0.27 -0.25 0.27 -0.24 0.27 -0.22 0.27 -0.2 0.28 -0.17 0.27 -0.15 0.28 -0.12 0.28 -0.08 0.29 -0.06 0.29 -0.01 0.3 0.03 0.31 0.07 0.33 0.12 0.33 0.17 0.35 0.22 0.37 0.29 0.38 0.34 0.4 -0.08 -0.04zm0 0.24l-0.05 0 -0.04 -0.04 0.09 0.04z" />
+                  </g>
+                  <path class="fil2" d="M5.36 9.64l-2.68 0 0.35 -0.55 1.77 0 0 -0.58 -1.4 0 0.35 -0.55 1.61 0 0 0.55 0.15 0 0.52 -4.81 -0.13 0 0.1 -0.13c-0.08,-0.07 -0.13,-0.18 -0.13,-0.3 0,-0.22 0.15,-0.39 0.36,-0.42l0.02 -0.07 0.04 -0.16 0.04 0.16 0.02 0.07c0.2,0.03 0.36,0.2 0.36,0.42 0,0.12 -0.05,0.23 -0.13,0.3l0.09 0.13 -0.12 0 0.48 4.81 0.18 0 0 -0.55 1.61 0 0.35 0.55 -1.39 0 0 0.58 1.76 0 0.36 0.55 -2.69 0 0 -0.55 -1.85 0 0 0.55z" />
+                  <rect class="fil2" x="2.83" y="11.15" width="6.91" height="0.68" rx="0.16" ry="0.16" />
+                  <g>
+                    <path class="fil2" d="M5.79 9.2c0.12,0 0.22,0.1 0.22,0.23 0,0.12 -0.1,0.22 -0.22,0.22 -0.12,0 -0.22,-0.1 -0.22,-0.22 0,-0.13 0.1,-0.23 0.22,-0.23z" />
+                    <path id="_1" class="fil3 str0" d="M5.79 9.2c0.12,0 0.22,0.1 0.22,0.23 0,0.12 -0.1,0.22 -0.22,0.22 -0.12,0 -0.22,-0.1 -0.22,-0.22 0,-0.13 0.1,-0.23 0.22,-0.23z" />
+                  </g>
+                  <g>
+                    <path class="fil2" d="M6.77 9.2c0.13,0 0.23,0.1 0.23,0.23 0,0.12 -0.1,0.22 -0.23,0.22 -0.12,0 -0.22,-0.1 -0.22,-0.22 0,-0.13 0.1,-0.23 0.22,-0.23z" />
+                    <path id="_1_0" class="fil3 str0" d="M6.77 9.2c0.13,0 0.23,0.1 0.23,0.23 0,0.12 -0.1,0.22 -0.23,0.22 -0.12,0 -0.22,-0.1 -0.22,-0.22 0,-0.13 0.1,-0.23 0.22,-0.23z" />
+                  </g>
+                </g>
+              </g>
+            </g>
+          </svg>
         </div>
-        <div class="grid grid-flow-col grid-cols-6 items-center">
-          <div class="col-span-2 text-xl">Jabatan</div>
-          <div class="col-span-4 h-12 rounded-2xl bg-white p-2 text-xl"></div>
+        <header>
+          <h1 class="text-xl font-semibold text-cyan-600">Panitia Penerimaan Santri Baru</h1>
+          <h2 class="text-3xl font-bold text-cyan-600">SMP-SMA Ali Maksum</h2>
+          <div class="text-lg font-medium">Tahun Pelajaran 2023/2024</div>
+        </header>
+      </div>
+      <div class="my-10 rounded-xl border bg-gray-50/70 p-6">
+        <h3 class="mb-10 text-center text-2xl font-bold">PANITIA</h3>
+        <div class="my-4 grid grid-flow-row gap-4">
+          <div class="">
+            <div class="text-lg mb-1">Nama</div>
+            <div class="h-14 rounded-2xl bg-white p-2 text-xl"></div>
+          </div>
+          <div class="">
+            <div class="text-lg mb-1">Jabatan</div>
+            <div class="h-14 rounded-2xl bg-white p-2 text-xl"></div>
+          </div>
         </div>
       </div>
     </div>
   </div>
 </div>
-</div>
-`
 
-setupCounter(document.querySelector('#counter'))
+`;
